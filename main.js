987d6465ac9d75594aea249440159b9704ebcfc3
Admin <6325484+SuperCheese21@users.noreply.github.com>
2017-07-31 14:27:11 -0700

    Used recursion to allow http requests to be asynchronous

--- main.js
+++ main.js
@@ -1,40 +1,25 @@
 const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
 const Fs = require('fs');
 const Promise = require('promise');
+const Request = require('./request.js')
 const Convert = require('./convert.js');
 
+var i = 0;
+var files = Fs.readdirSync('audio/');
 var speechToText = new SpeechToTextV1 ({
     username: '62ab9399-13af-48c8-9a48-96b8506fcae1',
     password: 'iXH0UsqFJsuy'
 });
-var sessionID = "";
-var audioFiles = Fs.readdirSync('audio/');
 
-speechToText.createSession({}, function(error, session) {
-    if (error)
-        console.log('Error:', error);
-    else {
-        console.log('Session ID: ' + session.session_id);
-        sessionID = session.session_id;
-    }
-});
+function getTranscript() {
+    if (i == files.length) return;
 
-var file = 'test1.flac';
-console.log('file: ' + file + ', i: ' + i);
-console.log('Requesting transcript for ' + file + '...');
+    var file = files[i++];
+    console.log('file: ' + file);
 
-var params = {
-    'session_id': sessionID,
-    'audio': Fs.createReadStream('audio/' + file),
-    'content_type': 'audio/flac'
-};
-speechToText.recognize(params, (error, transcript) => {
-    if (error) {
-        console.log('Error: ', error.message);
-    } else {
-        var fileName = file.split('.flac')[0];
-        var content = JSON.stringify(transcript, null, '\t');
+    Request.request(file, speechToText, () => {
+        getTranscript();
+    });
+}
 
-        Convert.convertJSON(fileName, content);
-    }
-});
+getTranscript();
--- request.js
+++ request.js
@@ -1,8 +1,28 @@
-const fs = require('fs');
+const Fs = require('fs');
 const Convert = require('./convert.js');
 
-var request = function(file, speechToText, params) {
-    console.log('To be implemented');
+var request = function(file, speechToText, callback) {
+    if (file.indexOf('.flac') !== -1) {
+        console.log('Requesting transcript for ' + file + '...');
+
+        var params = {
+            'audio': Fs.createReadStream('audio/' + file),
+            'content_type': 'audio/flac'
+        };
+        speechToText.recognize(params, (error, transcript) => {
+            if (error) {
+                console.log('Error: ', error.message);
+            } else {
+                var fileName = file.split('.flac')[0];
+                var content = JSON.stringify(transcript, null, '\t');
+
+                Convert.convertJSON(fileName, content);
+                callback();
+            }
+        });
+    } else {
+        callback();
+    }
 }
 
 module.exports = {
